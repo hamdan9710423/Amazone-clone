caa066ad3372ff7ef4b6c4e7aa9f02c498186723
Hamdan <161046085+hamdan9710423@users.noreply.github.com>
2024-07-09 16:36:27 +0500

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,372 @@
+*{
+    margin: 0;
+    font-family: arial;
+    border: border-box;
+}
+
+
+/* Panel 1 */
+/* Box 1 */
+
+.navbar{
+    height:60px;
+    background-color: #0f1111;
+    color:white;
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+}
+
+.nav-logo{
+    height: 50px;
+    width:100px;
+}
+
+.logo{
+    background-image:url('https://i.postimg.cc/jjym8pNW/amazon-logo.png');
+    background-size: cover;
+    height: 50px;
+    width: 100%;
+  
+}
+
+.border:hover{
+    border:2px solid white;
+}
+
+/* Box 2 */
+
+.add-icon{
+    display:flex;
+   
+}
+
+.add-first{
+    color:#cccccc;
+    font-size:  0.87rem;
+    margin-left:14px;
+}
+
+.add-second{
+    margin-left:3px;
+    font-size: 1rem;
+
+}
+
+
+/* Box 3 */
+
+.nav-search{
+    display: flex;
+    justify-content: space-evenly;
+    background-color: pink;
+    width:610px ;
+    height: 38px;
+    border-radius: 4px;
+}
+
+.nav-search:hover{
+    border: 2px solid orange;
+}
+
+.search-select{
+    background-color: #f3f3f3;
+    width: 50px;
+    text-align: center;
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    border: none;
+    font-size: 1rem;
+
+}
+
+.search-input{
+    width: 565px;
+    border: none;
+    font-size:1rem;
+    
+}
+
+
+.search-icon{
+    width: 45px;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.2rem;
+    background-color: #febd68;
+    border-top-right-radius: 4px ;
+    border-bottom-right-radius: 4px;
+    color:  #0f1111;
+}
+
+/* Box 4 and 5 */
+span{
+    font-size: 0.78rem;
+    font-weight: 700;
+}
+
+.list{
+    font-weight: 700;
+}
+
+/* Box 6 */
+ .cart i{
+    font-size: 1.8rem;
+    }
+
+.cart{
+    font-weight: 700;
+}
+
+
+/* Panel 2*/
+
+.panel-2{
+    background-color: #232F3E;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    color: #ffffff;
+    justify-content: space-evenly;
+
+}
+
+.p2{
+    padding:5px;
+}
+
+
+
+/* Box 1*/
+
+.All{
+    
+    color: #ffffff;
+    font-weight: 600;
+    padding: 5px;
+}
+
+.All i{
+   font-size: 20px;
+}
+
+.panel-ops{
+    display:flex;
+    width: 70%;
+    
+}
+
+.panel-ops p{
+    margin-left:15px;
+    font-size: 0.9rem;
+}
+
+.deals{
+    font-size:1rem ;
+    font-weight: 700;
+}
+
+
+/* Hero Section */
+
+.hero-sec{
+    background-image: url("https://i.postimg.cc/ZRV9SbC9/hero-image-3.jpg");
+    height: 380px;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+
+.hero-msg{
+    background-color: white;
+    height:40px;
+    width: 85%;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 30px;
+    
+}
+
+.hero-msg a{
+    color: #007185;
+    cursor: pointer;
+}
+
+
+/* Shop Section */
+
+.shop-section{
+    display: flex;
+    justify-content: space-evenly;
+    background-color: #e2e7e2;
+    flex-wrap: wrap;
+}
+
+.box{
+    height: 400px;
+    width:23%;
+    background-color: white;
+    padding: 20px 0px 15px;
+    margin-top: 15px;
+}
+
+.box-content{
+    margin-left: 1rem;
+    margin-right: 1rem;
+}
+
+.bg_img{
+    height: 300px;
+    background-size: cover;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+}
+
+.box-content a{
+    color: #007185;
+    cursor: pointer;
+}
+
+.box-content a:hover{
+    color: rgb(126, 48, 48);
+    text-decoration: underline;
+}
+
+/* Long Box */
+
+.box9_Long{
+    height: 250px;
+    width:97%;
+    background-color: white;
+    padding: 20px 0px 15px;
+    margin-top: 15px;
+    margin-bottom: 25px;
+    }
+
+.box9_Long h2{
+        display: inline;
+        padding-left: 15px;
+    }
+
+
+.box9_Long a{
+    color: #007185;
+    cursor: pointer;
+    padding-left: 20px;
+}
+
+.box9_Long a:hover{
+    color: rgb(126, 48, 48);
+    text-decoration: underline;
+}
+
+.books{
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 20px;
+
+}
+
+.img_book{
+    height: 180px;
+    background-size:cover;
+    width:11%;
+
+}
+
+   
+
+
+
+/* Footer */
+
+/* Foot-panel-1 */
+
+footer{
+    margin-top: 15px;
+}
+
+.foot-panel1{
+    background-color: #37575a;
+    color:white;
+    height:50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.85rem;
+}
+
+/* Foot-panel-2 */
+
+.foot-panel2{
+    background-color:#232F3E;
+    height: 350px;
+    color: white;
+    display: flex;
+    justify-content:space-evenly;
+
+
+}
+
+.foot-panel2 ul{
+    margin-top: 50px;
+    
+}
+
+ul a{
+    display: block;
+    font-size: 0.85rem;
+    margin-top: 15px;
+    color:#dddddd ;
+}
+
+/* Foot-panel-3 */
+
+.foot-panel3{
+    height:70px;
+    background-color:#232F3E;
+    border-top: 0.5px solid white;
+    display: flex;
+    justify-content:space-around;
+    align-items: center;
+
+}
+
+.logo{
+    background-image: url("https://i.postimg.cc/jjym8pNW/amazon-logo.png");
+    width: 100px;
+}
+
+/* Foot-panel 3*/
+
+.foot-panel4{
+    background-color: #0f1111;
+    height: 73px;
+    color: #dddddd;
+    font-size: 0.7rem;
+    text-align: center;
+}
+
+.pages{
+    padding-top: 15px;
+}
+
+.copyright{
+    padding-top:5px ;
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
